37727545dbb3503933e2c00d242b3e6a0a8d95b9
Carlos Daniel <carlosd.1199@gmail.com>
2019-11-12 17:06:30 -0300

    Passo Backward incluído

--- MLP/main.py
+++ MLP/main.py
@@ -46,21 +46,39 @@ class MLP():
             soma += w[i] * x[i]
         return soma
 
+    def calcular_gradiente_local(self, i, j):
+        soma = 0
+        for k in range(0, self.camadas[i + 1]):
+            soma += self.gradiente_local[i + 1][k] * self.peso_sinaptico[i + 1][k][j]
+        return soma
     def treinar(self):
         for amostra in self.entradas:
             for i in range(0, len(self.camadas)):
                 #passo foward
                 for j in  range(0, self.camadas[i]):
                     if i == 0:
-                        entrada = amostra
+                        entrada = amostra[0]
                     else:
                         entrada = self.saida[i - 1]
                     u = self.calcular_potencial(self.peso_sinaptico[i][j], entrada)
                     self.potencial_ativacao[i][j] = u
                     self.saida[i][j] = self.funcao_ativacao(u)
                 #passo backward
+            for i in range(len(self.camadas) - 1, 0, -1):
+                if i == 0:
+                    saida = amostra[0]
+                else:
+                    saida = self.saida[i - 1]
+                for j in range (0, self.camadas[i]):
+                    if i == len(self.camadas) - 1:
+                        self.gradiente_local[i][j] = (amostra[1][j] - self.saida[i][j]) * self.funcao_ativacao(self.potencial_ativacao[i][j], derivada=True)
+                    else:
+                        self.gradiente_local[i][j] = self.calcular_gradiente_local(i, j) * self.funcao_ativacao(self.potencial_ativacao[i][j], derivada=True)
+                    # self.gradiente_local[i][j] =  valor * self.funcao_ativacao(self.potencial_ativacao[i][j], derivada=True)
+                    for k in range(0, len(self.peso_sinaptico[i][j])):
+                        self.peso_sinaptico[i][j][k]  += self.taxa_aprendizagem * self.gradiente_local[i][j] * saida[k]
         print(self.peso_sinaptico)
         print(self.potencial_ativacao)
         print(self.saida)
-mlp = MLP([1, 3, 2], [[2, 2, 4, 1]], tangente_hiperbolica, 0.5, 0.0001)
+mlp = MLP([1, 3, 2], [[[2, 2, 4, 1], [1, 2]]], tangente_hiperbolica, 0.5, 0.0001)
 mlp.treinar()
